8d1293f3b03b1908761967270827cf755f771c68
griffgruffjava <C_Train@10.0.1.8>
2015-12-06 14:47:35 +0000

    added breadth-first search for matrixgraph part 2

--- test/MatrixGraphTest.java
+++ test/MatrixGraphTest.java
@@ -39,64 +39,99 @@ public class MatrixGraphTest {
     /**
      * Test of isEdge method, of class MatrixGraph.
      */
-    @Test
-    public void testIsEdge() {
-        System.out.println("isEdge");
-        int source = 0;
-        int dest = 0;
-        MatrixGraph instance = null;
-        boolean expResult = false;
-        boolean result = instance.isEdge(source, dest);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testIsEdge() {
+//        System.out.println("isEdge");
+//        int source = 0;
+//        int dest = 0;
+//        MatrixGraph instance = null;
+//        boolean expResult = false;
+//        boolean result = instance.isEdge(source, dest);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of insert method, of class MatrixGraph.
      */
+//    @Test
+//    public void testMatrixGraph() {
+//        System.out.println("matrix methods test");
+//        Edge edge0 = new Edge(0,3);
+//        Edge edge1 = new Edge(0,2);
+//        Edge edge2 = new Edge(0,1);
+//        Edge edge3 = new Edge(1,2);
+//        Edge edge4 = new Edge(2,4);
+//        MatrixGraph mg = new MatrixGraph(5, true);
+//        mg.insert(edge0);
+//        mg.insert(edge1);
+//        mg.insert(edge2);
+//        mg.insert(edge3);
+//        mg.insert(edge4);
+//        
+//        System.out.println(mg.toString());
+//        
+//        mg.remove(edge1);
+//        
+//        System.out.println(mg.toString());
+//        
+//        System.out.println(" (0,1) is an edge "+mg.isEdge(0, 1));
+//        System.out.println(" (2,4) is an edge "+mg.isEdge(2, 4));
+//        System.out.println(" (0,2) is an edge "+mg.isEdge(0, 2));
+//        
+//        
+//        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+//    }
+    
     @Test
-    public void testMatrixGraph() {
-        System.out.println("matrix methods test");
-        Edge edge0 = new Edge(0,3);
-        Edge edge1 = new Edge(0,2);
-        Edge edge2 = new Edge(0,1);
-        Edge edge3 = new Edge(1,2);
-        Edge edge4 = new Edge(2,4);
-        MatrixGraph mg = new MatrixGraph(5, true);
+    public void breadthFirstTraversalTest()
+    {
+        System.out.println("breadthFirstTraversal test");
+        Edge edge0 = new Edge(0,1);
+        Edge edge1 = new Edge(0,3);
+        Edge edge2 = new Edge(0,4);
+        Edge edge3 = new Edge(0,2);
+        Edge edge4 = new Edge(1,3);
+        Edge edge5 = new Edge(1,4);
+        Edge edge6 = new Edge(2,5);
+        Edge edge7 = new Edge(2,6);
+        Edge edge8 = new Edge(5,6);
+        Edge edge9 = new Edge(3,4);
+        
+        MatrixGraph mg = new MatrixGraph(7, false);
         mg.insert(edge0);
         mg.insert(edge1);
         mg.insert(edge2);
         mg.insert(edge3);
         mg.insert(edge4);
+        mg.insert(edge5);
+        mg.insert(edge6);
+        mg.insert(edge7);
+        mg.insert(edge8);
+        mg.insert(edge9);
         
-        System.out.println(mg.toString());
+//        mg.breadthFirstTraversal(0);
+//        System.out.println(mg.toString());
+        System.out.println("");
+        mg.breadthFirstTraversal(4);
         
-        mg.remove(edge1);
-        
-        System.out.println(mg.toString());
-        
-        System.out.println(" (0,1) is an edge "+mg.isEdge(0, 1));
-        System.out.println(" (2,4) is an edge "+mg.isEdge(2, 4));
-        System.out.println(" (0,2) is an edge "+mg.isEdge(0, 2));
-        
-        
-        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
     }
+            
 
     /**
      * Test of remove method, of class MatrixGraph.
      */
-    @Test
-    public void testRemove() {
-        System.out.println("remove");
-        Edge edge = null;
-        MatrixGraph instance = null;
-        instance.remove(edge);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testRemove() {
+//        System.out.println("remove");
+//        Edge edge = null;
+//        MatrixGraph instance = null;
+//        instance.remove(edge);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 //
 //    /**
 //     * Test of breadthFirstTraversal method, of class MatrixGraph.
